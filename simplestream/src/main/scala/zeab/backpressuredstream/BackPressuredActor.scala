521bbd47876ade1c2ae1ea19cf6dd85df09b17f2
kevin downs <pyrosrppltoo@gmail.com>
2018-12-22 23:25:18 -0800

    Update BackPressuredActor.scala

--- simplestream/src/main/scala/zeab/backpressuredstream/BackPressuredActor.scala
+++ simplestream/src/main/scala/zeab/backpressuredstream/BackPressuredActor.scala
@@ -1,22 +1,26 @@
 package zeab.backpressuredstream
 
 //Imports
+import zeab.backpressuredstream.BackPressuredMessages.{Ack, Complete, Init, StreamFailure}
+//Java
 import java.util.concurrent.atomic.LongAdder
-
+//Akka
 import akka.actor.Actor
 import akka.event.{Logging, LoggingAdapter}
-import zeab.backpressuredstream.BackPressuredMessages.{Ack, Init}
 
 class BackPressuredActor extends Actor{
 
   //Actor Settings
   val actorLog: LoggingAdapter = Logging(context.system, this)
 
+  //Receive
   def receive: Receive = count()
 
+  //Behaviors
   def count(c: Int = 0): Receive = {
-    case Init =>
-      sender ! Ack
+    case Init => sender ! Ack
+    case Complete => actorLog.info("Back Pressured Completed")
+    case StreamFailure(ex) => actorLog.error(ex.toString)
     case m: LongAdder =>
       sender ! Ack
       m.increment()
