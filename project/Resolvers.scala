7ce769d427f04329746ad072f41646c3be0e2b44
kevin downs <pyrosrppltoo@gmail.com>
2019-01-17 00:16:58 -0800

    Update Resolvers.scala

--- project/Resolvers.scala
+++ project/Resolvers.scala
@@ -5,10 +5,9 @@ import sbt._
 
 object Resolvers {
 
-  val zeabArtifactoryLocal = "Artifactory" at "http://192.168.1.144:8081/artifactory/ivy-dev-local/"
   val zeabArtifactoryRemote = "Artifactory" at "http://67.185.79.236:8081/artifactory/ivy-dev-local/"
 
   //TODO There has got to be a better way to do this than the way I just put it together
-  val allResolvers: Seq[Def.Setting[_]] = Seq(resolvers += zeabArtifactoryLocal) ++ Seq(resolvers += zeabArtifactoryRemote)
+  val allResolvers: Seq[Def.Setting[_]] = Seq(resolvers += zeabArtifactoryRemote)
 
 }
